379ee49bd434aa519f16f136a3bd87dc9283e932
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-09-10 13:53:17 +0200

    fix design layout

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const container = document.querySelector(".container");
-
-
-// box.setAttribute("style", "width: 16px; height: 16px; background: blue;")
 const size = 600;
 let num = 16;
+let borderEnabled = true;
+let hoverColor = "black";
 
+const colorPicker = document.getElementById("colorPicker");
+const input = document.getElementById("input");
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-    num = prompt('Enter number of squares, maximun 100');
+const changeGrid = document.querySelector(".change-grid");
+changeGrid.addEventListener("click", () => {
+    num = parseInt(input.value);
     if (num <= 0 || num > 100) return;
-   removeBoard();
-   createBoxes(num);
+    removeBoxes();
+    createBoxes(num);
 });
 
- function createBoxes(num) {
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+    removeBoxes();
+    createBoxes(16);
+});
+
+const changeColor = document.querySelector(".change-color");
+changeColor.addEventListener("click", () => {
+    colorPicker.click();
+    colorPicker.addEventListener("input", (e) => {
+        hoverColor = e.target.value;
+    });
+
+});
+
+const borderButton = document.querySelector(".border");
+borderButton.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".box");
+    squares.forEach(square => {
+        if (borderEnabled) {
+            square.style.border = 'none';
+        } else {
+            square.style.border = '1px solid #dd976f';
+        }
+    });
+    borderEnabled = !borderEnabled;
+});
+
+function createBoxes(num) {
     const pixelSize = 400 / num;
+
     for (let i = 0; i < num; i++) {
         let div = document.createElement("div");
         div.classList.add("div");
+
         for (let j = 0; j < num; j++) {
             let square = document.createElement("div");
             square.setAttribute("class", "box");
@@ -26,33 +57,22 @@ button.addEventListener("click", () => {
                 width:${pixelSize}px; height:${pixelSize}px; 
                 box-sizing: border-box; border: 1px solid #dd976f`);
             div.append(square);
+
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = hoverColor;
+            });
+
         }
+
         container.append(div);
     }
 }
 
-const removeBoard = () => {
+
+const removeBoxes = () => {
     while (container.lastElementChild) {
-      container.removeChild(container.lastElementChild);
+        container.removeChild(container.lastElementChild);
     }
-  };
+};
 
 createBoxes(16);
-
-// function createGrid(num){
-
-//     let square = document.createElement("div");
-//     square.setAttribute("style", "width: 10; height: 10; border: 2px solid black;");
-    
-
-
-//  for (let i = 0; i < num; i++)
-//     square = num;
-    
-
-//     container.appendChild(square);
-// }
-
-
-
-// createGrid(16);
