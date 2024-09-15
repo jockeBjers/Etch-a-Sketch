dcaea3bf237d97055e5b0785407688bf5dc535d4
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-09-15 09:25:07 +0200

    clean code for better readability

--- script.js
+++ script.js
@@ -1,92 +1,86 @@
-const container = document.querySelector(".container");
-const size = 400;
+const gridSize = 400;
 let num = 16;
-let borderEnabled = true;
+let isBorderVisible = true;
 let hoverColor = "black";
-let clickmode = false;
-let isMouseDown = false; 
+let isMouseDown = false;
 
-const colorPicker = document.getElementById("colorPicker");
+const container = document.querySelector(".container");
+const borderButton = document.querySelector(".border");
+const resetGrid = document.querySelector(".reset");
 const input = document.getElementById("input");
-
 const changeGrid = document.querySelector(".change-grid");
+
+// changing grid size depending on input
 changeGrid.addEventListener("click", () => {
     num = parseInt(input.value);
     if (num <= 0 || num > 100) return;
     removeBoxes();
-    createBoxes(num);
+    generateGrid(num);
 });
 
-const reset = document.querySelector(".reset");
-reset.addEventListener("click", () => {
+resetGrid.addEventListener("click", () => {
     removeBoxes();
-    createBoxes(16);
+    generateGrid(16);
 });
-
-const pickr = Pickr.create({
+// Initializing the pickr library
+const colorPicker = Pickr.create({
     el: '#color-picker-container',
-    theme: 'classic', 
-    default: '#000000', 
-    components: {   
-        opacity: true,
+    theme: 'classic',
+    default: '#000000',
+    components: {
         hue: true,
         inline: true,
-        interaction: {
-        }
     }
 });
 
-pickr.show();
+colorPicker.show();
 
-pickr.on('hide', (instance) => {
-    instance.show(); 
+colorPicker.on('hide', (instance) => {
+    instance.show();
 });
 
-pickr.on('change', (color) => {
-    hoverColor = color.toHEXA().toString();  
+colorPicker.on('change', (color) => {
+    hoverColor = color.toHEXA().toString();
 });
 
+// tracking mouse action
 document.addEventListener("mousedown", (e) => {
-    if (e.button === 0) { 
+    if (e.button === 0) {
         isMouseDown = true;
-    } 
+    }
 });
 
 document.addEventListener("mouseup", (e) => {
-    if (e.button === 0) { 
+    if (e.button === 0) {
         isMouseDown = false;
-    } 
-});
-const borderButton = document.querySelector(".border");
-borderButton.addEventListener("click", () => {
-    const squares = document.querySelectorAll(".box");
-    squares.forEach(square => {
-        if (borderEnabled) {
-            square.style.border = 'none';
-        } else {
-            square.style.border = '1px solid #dd976f';
-        }
-    });
-    borderEnabled = !borderEnabled;
+    }
 });
 
-function createBoxes(num) {
-    const pixelSize = size / num;
-    
+//Toggle border on / off 
+borderButton.addEventListener("click", () => {
+    isBorderVisible = !isBorderVisible;
+    document.querySelectorAll(".box").forEach(square => {
+        square.classList.toggle('no-border', !isBorderVisible);
+    });
+});
+
+//creating a grid of boxes
+function generateGrid(num) {
+    const pixelSize = gridSize / num;
+
     for (let i = 0; i < num; i++) {
-        let div = document.createElement("div");
-        div.classList.add("div");
+        let row = document.createElement("div");
+        row.classList.add("row");
 
         for (let j = 0; j < num; j++) {
             let square = document.createElement("div");
-            square.setAttribute("class", "box");
-            square.setAttribute("style", `background-color: #e9cebe;
-                width:${pixelSize}px; height:${pixelSize}px; 
-                box-sizing: border-box; border: 1px solid #dd976f`);
-            div.append(square);
+            square.classList.add("box");
+            square.style.width = `${pixelSize}px`;
+            square.style.height = `${pixelSize}px`;
 
-              // Change color on press
-              square.addEventListener("mousedown", () => {
+
+            // Change color on press
+            square.addEventListener("mousedown", () => {
                 square.style.backgroundColor = hoverColor;
             });
 
@@ -96,8 +90,9 @@ function createBoxes(num) {
                     square.style.backgroundColor = hoverColor;
                 }
             });
+            row.appendChild(square);
         }
-        container.append(div);
+        container.appendChild(row);
     }
 }
 
@@ -107,4 +102,6 @@ const removeBoxes = () => {
     }
 };
 
-createBoxes(16);
+
+//Initialize with a default grid of 16x16
+generateGrid(16);
